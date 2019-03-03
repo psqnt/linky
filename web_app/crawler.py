33f9c84bb563f436fc29da964f058ec43a593665
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2019-03-03 12:52:40 -0500

    initial commit, web crawler and very basic index search -> search engine

--- web_app/crawler.py
+++ web_app/crawler.py
@@ -1,6 +1,9 @@
 import sqlite3
 import click
 import requests
+from time import sleep
+from random import randint
+from bs4 import BeautifulSoup
 from flask import current_app, g
 from flask.cli import with_appcontext
 from werkzeug.exceptions import abort
@@ -8,30 +11,110 @@ from web_app.db import get_db
 
 
 @click.command('crawl')
+@click.argument('seed')
 @with_appcontext
-def crawl_command():
+def crawl_command(seed):
     """Clear the existing data and create new tables."""
-    crawl()
-    click.echo('crawled new links.')
+    main(seed)
+    click.echo('Ran out of links to crawl.')
 
 
 def init_app(app):
     app.cli.add_command(crawl_command)
 
 
-def make_request(url):
-    print(url)
-    response = requests.get(url)
-    print(response.json())
-
-
-def crawl():
+def add_page_to_index(link, content):
+    soup = BeautifulSoup(content, 'html.parser')
+    try:
+        title = soup.title.string.lower()
+    except:
+        title = "none"
     db = get_db()
-    most_recent_link = db.execute(
-        'SELECT * FROM links ORDER BY id DESC LIMIT 1'
-    ).fetchone()
-    print(most_recent_link)
-    if most_recent_link is None:
-        most_recent_link = 'http://joerogan.net'
+    db.execute(
+        'INSERT INTO websites (link, title) VALUES (?, ?)', (link, title)
+    )
+    db.commit()
 
-    make_request(most_recent_link)
+
+def get_links(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    links = []
+    for link in soup.find_all('a'):
+        try:
+            if 'http' in link.get('href'):
+                links.append(link.get('href'))
+        except Exception as e:
+            pass
+    return set(links)
+
+
+def add_tocrawl(link):
+    db = get_db()
+    db.execute(
+        'INSERT INTO tocrawl (link) VALUES (?)', (link,)
+    )
+    db.commit()
+
+
+def get_seed():
+    db = get_db()
+    last_saved_website = db.execute(
+        'SELECT link FROM tocrawl ORDER BY id DESC LIMIT 1'
+    ).fetchone()
+    return last_saved_website['link']
+
+
+def in_database(url):
+    db = get_db()
+    website = db.execute(
+        'SELECT link FROM websites WHERE link = (?)', (url,)
+    ).fetchone()
+    if website is None:
+        return False
+    return True
+
+
+def union(a, b):
+    for link in b:
+        if link not in a:
+            a.append(link)
+
+
+def crawl(seed):
+    """
+    seed is a url
+    """
+    to_crawl = [seed]
+    crawled = []
+    index = []
+    count = 0
+    avoid = ['.zip', '.pdf', '.png', '.jpg', '.jpeg', '.doc', '.mp4', '.mov']
+    while to_crawl and count < 100:
+        url = to_crawl.pop()
+        if 'facebook' not in url:
+            if len(url) < 50:
+                if url not in crawled:
+                    if url[-4:] not in avoid:
+                        if not in_database(url):
+                            print(f'crawling: {url}')
+                            try:
+                                response = requests.get(url, timeout=20)
+                            except Exception as e:
+                                pass
+                            content = response.content
+                            add_page_to_index(url, content)
+                            links = get_links(content)
+                            union(to_crawl, links)
+                            crawled.append(url)
+                            #count += 1
+        sleep(1)
+    add_tocrawl(to_crawl[0])
+    return crawled
+
+
+def main(seed):
+    if seed == 'restart':
+        print('restarting from database')
+        crawl(get_seed())
+    else:
+        crawl(seed)
--- web_app/index.py
+++ web_app/index.py
@@ -8,10 +8,49 @@ from web_app.db import get_db
 bp = Blueprint('index', __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 def index():
+
+    # if POST
+    if request.method == 'POST':
+        return results(request.form['search'])
+    else:
+        db = get_db()
+        links = db.execute(
+            'SELECT link, title FROM websites'
+        ).fetchall()
+        link = links[0]
+        count = len(links)
+        return render_template('index/index.html', link=link, count=count)
+
+
+@bp.route('/all')
+def index_all():
     db = get_db()
-    links = db.execute(
-        'SELECT id, link, title FROM links'
+    results = db.execute(
+        'SELECT link, title FROM websites'
     ).fetchall()
-    return render_template('index/index.html', links=links)
+    return render_template('index/results.html', results=results)
+
+
+@bp.route('/results')
+def results(search):
+    words = search.lower().split()
+    db = get_db()
+    results = []
+    db_results = db.execute(
+        'SELECT link, title FROM websites',
+    ).fetchall()
+
+    # brute FORCE!
+    for word in words:
+        for r in db_results:
+            if word in r['title']:
+                results.append(r)
+
+    if not results:
+        flash('No results found!')
+        return redirect('/')
+    else:
+        # display results
+        return render_template('index/results.html', results=results)
